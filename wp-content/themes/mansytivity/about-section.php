fedad5299cb3bdfec9e49dd5f2cd6c93c457aad4
Al Mansy Balangue <almansybalangue@yahoo.com>
2020-02-01 08:08:17 -0500

    Added SVG
    Added SVG

--- wp-content/themes/mansytivity/about-section.php
+++ wp-content/themes/mansytivity/about-section.php
@@ -1,5 +1,6 @@
 <section id="about">
     <div id="about-content">
         <h1>It's me!</h1>
+        <img src="http://localhost/mansytivity/wp-content/uploads/2020/02/mansy-nametag.svg" />
     </div>
 </section>
