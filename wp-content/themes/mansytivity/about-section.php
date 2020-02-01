254625186e2510e49554d0a2e1bc67102beb955d
Al Mansy Balangue <almansybalangue@yahoo.com>
2020-02-01 09:16:27 -0500

    Updated About Section
    Updated About Section

--- wp-content/themes/mansytivity/about-section.php
+++ wp-content/themes/mansytivity/about-section.php
@@ -1,6 +1,26 @@
 <section id="about">
     <div id="about-content">
-        <h1>It's me!</h1>
-        <img src="http://localhost/mansytivity/wp-content/uploads/2020/02/mansy-nametag.svg" />
+        <div class="about-content-left">
+            <img class="avatar-nametag"
+                src="http://localhost/mansytivity/wp-content/uploads/2020/02/mansy-name-tag.svg" />
+            <img class="avatar-photo"
+                src="http://localhost/mansytivity/wp-content/uploads/2020/02/mansy-profile-photo.jpg" />
+        </div>
+        <div class="about-content-right">
+            <h1>Al Mansy Balangue</h1>
+
+            <div class="data">
+                <div class="address about-data">10 Exchange St Portland, Maine 04101</div>
+                <div class="email about-data">almansybalangue@gmail.com</div>
+                <div class="number about-data">+1 207 2567 869</div>
+            </div>
+            <div class="separator">&nbsp;</div>
+            <div class="education">
+                <div class="course about-data">Bachelors of Science in Computer Engineering</div>
+                <div class="email about-data">Oracle Certified Associate, JAVA SE7 Programmer</div>
+                <div class="number about-data">Best Computer Engineering Student SY 2007-2012</div>
+                <div class="number about-data">Department of Science and Technology Scholar</div>
+            </div>
+        </div>
     </div>
 </section>
